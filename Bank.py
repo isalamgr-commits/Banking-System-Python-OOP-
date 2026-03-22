2e349b2bcc802fb83c77bc40aaea4e257f06e10a
Salam <isalamgr@gmail.com>
2026-03-22 13:33:47 -0500

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,18 +1,55 @@
+from CuentaAhorro import SavingAccount
+from CuentaCorriente import CurrentAccount
+
 class Bank:
-    def menu(self):
-        print("1. NEW CLIENT")
-        print("2. CREATE ACCOUNT")
-        print("3. SEARCH ACCOUNT")
-        print("4. TRANSFER")
+    def __init__(self):
+        self.clients = []
+        self.accounts = []
         
-    def create_client(self):
-        pass
+    def create_client(self, name, email, id):
+        cliente = Cliente(name, email, id) # type: ignore
+        self.clients.append(cliente)
 
     def create_account(self):
-        pass
+        client_id = input("ENTER THE CLIENT ID: ")
+        for client in self.clients:
+           if client.id == client_id:
+                client_found = client
+                break
+           
+           if not client_found:
+               print("CLIENT NOT FOUND.")
+               return
+        
+        print("ACCOUNT TYPE:")
+        print("1 SAVING ACCOUNT")
+        print("2 CURRENT ACCOUNT")
+
+        account_type = input("ENTER: ")
+        if account_type == "1":
+            cuenta = SavingAccount()
+        elif account_type == "2":
+            cuenta = CurrentAccount()
+
+        client.accounts.append(cuenta)
+        client_found.cuentas.append(cuenta)
 
     def search_account(self):
-        pass
+        n_account = input("ENTER ACCOUNT NUMBER: ")
+        for account in self.accounts:
+            if account.n_account == n_account:
+                print("ACCOUNT FOUND.")
+                account.show_info()
+                return account
+        
+        print("ACCOUNT NOT FOUND.")
+        return None
 
     def transfer(self):
-        pass
+        cuenta_origen = input("ENTER ACCOUNT NUMBER: ")
+        for account in self.accounts:
+            if account.cuenta_origen == cuenta_origen:
+                account.show_info()
+                print("ENTER ACCOUNT TO TRANFER: ")
+            else:
+                print("ACCOUNT NOT FOUND.")
