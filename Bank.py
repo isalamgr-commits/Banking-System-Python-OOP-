d6b63a2762781218473d7a177c8714d401b5b9ee
Salam <isalamgr@gmail.com>
2026-03-30 08:35:29 -0500

    Add Bank.py

--- Bank.py
+++ Bank.py
@@ -31,7 +31,22 @@ class Bank:
          if client.id == id:
              return client
       return None
-           
+    
+    def create_pin(self):
+        while True:
+            pin = input("CREATE 4-DIGIT PIN: ")
+
+            if not pin.isdigit() or len(pin) != 4:
+                print("INVALID PIN.")
+                continue
+            
+            confirm = input("CONFIRM PIN: ")
+
+            if pin != confirm:
+                print("PIN DOES NOT MATCH")
+                continue
+            return pin
+       
     def create_account(self):
         client_id = input("ENTER THE CLIENT ID: ")
         client_found= None
@@ -44,10 +59,7 @@ class Bank:
         if not client_found:
             print("CLIENT NOT FOUND.")
             return
-        
-        n_account = self.generate_account_number()
-        balance = float(input("INITIAL BALANCE:"))
-        
+
         print("ACCOUNT TYPE:")
         print("1 SAVING ACCOUNT")
         print("2 CURRENT ACCOUNT")
@@ -55,10 +67,40 @@ class Bank:
         account_type = input("ENTER: ")
 
         if account_type == "1":
+            while True:
+                try:
+                  balance = float(input("INITIAL BALANCE:"))
+                  break
+                except:
+                    print("INVALID AMOUNT")
+            
+            n_account = self.generate_account_number()
+            
             cuenta = SavingAccount(n_account, balance, client_found)
 
         elif account_type == "2":
-            overdraft = 1000
+            while True:
+              entrada = input("INITIAL BALANCE(MIN $500) or x to cancel: ")
+              
+              if entrada.lower() == "x":
+                  print("CANCELLED")
+                  return
+              
+              try:
+                  balance = float(entrada)
+
+                  if balance < 500:
+                      print("MINIMUN $500 REQUIRED.")
+                      continue
+                  break
+              
+              except:
+                  print("INVALID AMOUNT")
+                  return
+
+            n_account = self.generate_account_number()
+
+            overdraft = 500
             cuenta = CurrentAccount(n_account, balance, client_found, overdraft)
 
         else:
@@ -69,6 +111,7 @@ class Bank:
         client_found.accounts.append(cuenta)
         
         print("ACCOUNT CREATED SUCCESSFULLY.")
+        pin = self.create_pin()
         print(f"ACCOUNT NUMBER: {n_account}")
     
     def search_account(self):
@@ -95,8 +138,18 @@ class Bank:
             print("ACCOUNR NOT FOUND.")
             return
         
+        pin_input = input("ENTER YOUR PIN: ")
+
+        if pin_input != cuenta_origen.pin:
+            print("INVALID PIN")
+            return
+
         destino_num = input("ENTER DESTINATION ACCOUNT: ")
         cuenta_destino = None
+
+        if num_origen == destino_num:
+            print("CANNOT TRANSFER TO THE SAME ACCOUNT")
+            return
         
         for account in self.accounts:
            if account.n_account == destino_num:
@@ -106,8 +159,17 @@ class Bank:
         if not cuenta_destino:
             print("DESTINATION NOT FOUND")
             return
-        
-        amount = float(input("ENTER AMOUNT: "))
+        while True:
+            try:
+                amount = float(input("ENTER AMOUNT: "))
+
+                if amount <= 0:
+                    print("INVALID AMOUNT")
+                    continue
+                break
+
+            except:
+                print("INVALID INPUT")
 
         if not cuenta_origen.withdraw(amount):
             print("TRANSFER FAILED")
